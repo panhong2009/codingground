782ff6b806333d97eb1120121e83e3a9c79976d1
panhong2009 <1392947487@qq.com>
2016-05-12 01:09:08 +0800

    new commit

--- noneiao.js
+++ noneiao.js
@@ -1,6 +1,7 @@
 function noneio(argu,bb/*,function(){console.log('This text is in callback function');}*/){
 	setTimeout(function(){console.log(argu);},5000)
-	console.log(argu);
 	bb(); 
+	console.log(argu);
+	
 }
 noneio('This is a lonly function',function(){console.log('This text is in callback function');});
