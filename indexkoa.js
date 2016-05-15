d04e5cbab726b53431d696edeed8c132cee1e00f
panhong2009 <1392947487@qq.com>
2016-05-15 22:22:21 +0800

    KOA example

--- indexkoa.js
+++ indexkoa.js
@@ -1,11 +1,13 @@
 var koa = require('koa');
+console.log(koa);
 var app = koa();
 
 // logger
 
 app.use(function *(next){
   var start = new Date;
-  yield next;
+  if(start) return yield next;
+ // yield next;
   var ms = new Date - start;
   console.log('%s : %s - %s', this.method, this.url, ms);
 });
@@ -13,7 +15,8 @@ app.use(function *(next){
 // response
 
 app.use(function *(){
-  this.body = 'Hello World';
+  this.body = yield [1,2,3,4];//'Hello World';
+ // console.log(~~this.cookies.get(secure));
 });
 
 app.listen(3000);
